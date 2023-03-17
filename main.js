1116e113451e36a6abe6a60d8ef769784a99c3f7
Wendesday36 <bertazoeanna@gmail.com>
2023-03-17 12:54:14 +0100

    keprekattintas

--- main.js
+++ main.js
@@ -11,13 +11,25 @@ const LIST = [
 ];
 $(function () {
   /**console.log("barmi") Selection.append('')*/
-  const felsoElem = $("#felso");
-  osszeAllit();
+  const FELSOELEM = $("#felso");
+  let tartalom = osszeAllit();
+  FELSOELEM.append(tartalom);
+  /**kiskepekre kattintas */
+  //fogd meg a kskepeket,adj hozza esemeny kezelot
+  const FELSOKEPEK = $("#felso img");
+  FELSOKEPEK.on("click",kepreKattintas)
 });
+function kepreKattintas() {
+   console.log(event.target)
+   
+
+  
+}
 function osszeAllit() {
   let txt = "";
   for (let index = 0; index < LIST.length; index++) {
     txt += `<div><img src = "${LIST[index]}" alt = ""></div>`;
   }
   console.log(txt);
+  return txt;
 }
