ab65a568047ff92a64d86dae1fa6341f75b9c317
Wendesday36 <bertazoeanna@gmail.com>
2023-03-17 12:14:41 +0100

    kepek

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 /**window.addEventListener("load",function(){
 
 })*/
-
+const LIST = [
+  "kepek/kep1.jpg",
+  "kepek/kep2.jpg",
+  "kepek/kep3.jpg",
+  "kepek/kep4.jpg",
+  "kepek/kep5.jpg",
+  "kepek/kep6.jpg",
+];
 $(function () {
-    console.log("barmi")
+  console.log("barmi");
 });
