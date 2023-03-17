a6c8072bb0d0e9af21eb5f643d8766d0044f5fc0
Wendesday36 <bertazoeanna@gmail.com>
2023-03-17 12:32:02 +0100

    txt,osszeAllit

--- main.js
+++ main.js
@@ -10,5 +10,14 @@ const LIST = [
   "kepek/kep6.jpg",
 ];
 $(function () {
-  console.log("barmi");
+  /**console.log("barmi") Selection.append('')*/
+  const felsoElem = $("#felso");
+  osszeAllit();
 });
+function osszeAllit() {
+  let txt = "";
+  for (let index = 0; index < LIST.length; index++) {
+    txt += `<div><img src = "${LIST[index]}" alt = ""></div>`;
+  }
+  console.log(txt);
+}
