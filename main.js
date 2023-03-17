a537641854f6740514de0944225ac818e80754b6
Wendesday36 <bertazoeanna@gmail.com>
2023-03-17 13:35:16 +0100

    visszaallas

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ const LIST = [
   "kepek/kep5.jpg",
   "kepek/kep6.jpg",
 ];
-const db = 0
-const KIVALASZTOTTKEPEK = 
+let db = 0;
+const KIVALASZTOTTKEPEK = []
 $(function () {
   /**console.log("barmi") Selection.append('')*/
   const FELSOELEM = $("#felso");
@@ -25,25 +25,23 @@ $(function () {
   /**kiskepekre kattintas */
   //fogd meg a kskepeket,adj hozza esemeny kezelot
   const FELSOKEPEK = $("#felso img");
- 
 
   FELSOKEPEK.on("click", kepreKattintas);
 });
 function kepreKattintas(event) {
   const aktualisKep = event.target;
-  console.log (aktualisKep.id)
+  console.log(aktualisKep.id);
   console.log($(aktualisKep).attr("id"));
   aktualisKep.src = LIST[aktualisKep.id];
-  KIVALASZTOTTKEPEK.push($(aktualisKep).attr("id"))
+  KIVALASZTOTTKEPEK.push($(aktualisKep).attr("id"));
   db++;
-  if (db == 0) {
+  if (db == 2) {
     db = 0;
     visszaFordit();
-    KIVALASZTOTTKEPEK.pop
-    KIVALASZTOTTKEPEK.pop
+    
+  }
 }
-}
-function osszeAllit() { 
+function osszeAllit() {
   let txt = "";
   for (let index = 0; index < LIST.length; index++) {
     txt += `<div><img src = "kepek/hatter.jpg" alt = "" id ="${index}"></div>`;
@@ -51,8 +49,17 @@ function osszeAllit() {
   console.log(txt);
   return txt;
 }
-function visszaFordit(){
-    console.log(KIVALASZTOTTKEPEK)
-    
-    aktualisKep,src = "kepek/hatter.jpg"
+function visszaFordit() {
+  console.log(KIVALASZTOTTKEPEK);
+
+  setTimeout(function () {
+    const FELSOKEPEK = $("#felso img");
+    let aktualisKep = FELSOKEPEK.eq(KIVALASZTOTTKEPEK[0]);
+    console.log(aktualisKep);
+    $(aktualisKep).attr("src", "kepek/hatter.jpg");
+    aktualisKep = FELSOKEPEK.eq(KIVALASZTOTTKEPEK[1]);
+    $(aktualisKep).attr("src", "kepek/hatter.jpg");
+    KIVALASZTOTTKEPEK.pop();
+    KIVALASZTOTTKEPEK.pop();
+  }, 1000);
 }
